27f38376898e0e47e5535a89b54e4580f8979c87
CsabaVeres <cs_veres@yahoo.com>
2024-12-01 19:59:24 +0100

    Update Adatok_beszurasa.sql

--- Adatok_beszurasa.sql
+++ Adatok_beszurasa.sql
@@ -1,9 +1,9 @@
 
 -- Vásárlók feltöltése
-INSERT INTO vasarlok (nev, email, cim, varos) VALUES
-('Kovács Péter', 'peter.kovacs@gmail.com', 'Budapest, Kossuth tér 1.', 'Budapest'),
-('Nagy Anna', 'anna.nagy@gmail.com', 'Debrecen, Kossuth utca 10.', 'Debrecen'),
-('Tóth László', 'laszlo.toth@gmail.com', 'Szeged, Petőfi utca 5.', 'Szeged');
+-- INSERT INTO vasarlok (nev, email, cim, varos) VALUES
+-- ('Kovács Péter', 'peter.kovacs@gmail.com', 'Budapest, Kossuth tér 1.', 'Budapest'),
+-- ('Nagy Anna', 'anna.nagy@gmail.com', 'Debrecen, Kossuth utca 10.', 'Debrecen'),
+-- ('Tóth László', 'laszlo.toth@gmail.com', 'Szeged, Petőfi utca 5.', 'Szeged');
 
 -- Vásárlók feltöltése (tömeges)
 INSERT INTO vasarlok (nev, email, cim, varos)
@@ -14,10 +14,10 @@ SELECT
 FROM generate_series(1, 50000) i;
 
 -- Könyvek feltöltése
-INSERT INTO konyvek (cim, szerzo, ar, keszlet) VALUES
-('A tenger szívében', 'Herman Melville', 3500.00, 50),
-('A nagy Gatsby', 'F. Scott Fitzgerald', 4500.00, 30),
-('A 451 fok Fahrenheit', 'Ray Bradbury', 2500.00, 20);
+-- INSERT INTO konyvek (cim, szerzo, ar, keszlet) VALUES
+-- ('A tenger szívében', 'Herman Melville', 3500.00, 50),
+-- ('A nagy Gatsby', 'F. Scott Fitzgerald', 4500.00, 30),
+-- ('A 451 fok Fahrenheit', 'Ray Bradbury', 2500.00, 20);
 
 -- Könyvek feltöltése (például 10 000 különböző könyv)
 INSERT INTO termekek (cim, szerzo, ar, keszlet)
@@ -29,7 +29,7 @@ SELECT
 FROM generate_series(1, 10000) i;
 
 -- Rendelések feltöltése
-INSERT INTO rendelesek (vasarlo_id) VALUES (1), (2);
+-- INSERT INTO rendelesek (vasarlo_id) VALUES (1), (2);
 
 -- Rendelések feltöltése (tömeges)
 INSERT INTO rendelesek (vasarlo_id, Datum, statusz)
@@ -37,17 +37,22 @@ SELECT (RANDOM() * 49999+1)::INT, NOW() - (INTERVAL '1 day' * (RANDOM() * 100)::
 FROM generate_series(1, 100000);
 
 -- Rendelés tételek feltöltése
-INSERT INTO rendeles_tetelek (rendeles_id, konyv_id, mennyiseg, egysegar) VALUES
-(1, 1, 2, 3500.00),
-(1, 2, 1, 4500.00),
-(2, 3, 1, 2500.00);
+-- INSERT INTO rendeles_tetelek (rendeles_id, konyv_id, mennyiseg, egysegar) VALUES
+-- (1, 1, 2, 3500.00),
+-- (1, 2, 1, 4500.00),
+-- (2, 3, 1, 2500.00);
 
 -- Rendelés tételek feltöltése
-INSERT INTO rendeles_tetelek (rendeles_id, termek_id, mennyiseg)
-SELECT (RANDOM() * 99999+1)::INT, (RANDOM() * 9999+1)::INT, (RANDOM() * 10 + 1)::INT
+INSERT INTO rendeles_tetelek (rendeles_id, konyv_id, mennyiseg, egysegar)
+SELECT (RANDOM() * 99999+1)::INT, (RANDOM() * 9999+1)::INT, (RANDOM() * 9999+1)::INT, (RANDOM() * 10 + 1)::INT
 FROM generate_series(1, 1000000);
 
 -- Számlák feltöltése
-INSERT INTO szamlak (rendeles_id, osszeg, fizetesi_mod) VALUES
-(1, 11500.00, 'Készpénz'),
-(2, 2500.00, 'Bankkártya');
+--- INSERT INTO szamlak (rendeles_id, osszeg, fizetesi_mod) VALUES
+-- (1, 11500.00, 'Készpénz'),
+-- (2, 2500.00, 'Bankkártya');
+
+-- Számlák feltöltése (tömeges)
+INSERT INTO szamlak (rendeles_id, osszeg, fizetesi_mod)
+SELECT (RANDOM() * 99999+1)::INT, (RANDOM() * 9999+1)::INT
+FROM generate_series(1, 1000000);
